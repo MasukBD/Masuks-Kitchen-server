a267486e01b23e69c601ddf0b59dc78b0e52e9e9
MasukBD <imahfuz2020@outlook.com>
2023-12-05 01:26:39 +0600

    build separate  jwt middleware for verify token and verify admin and use them various way

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const jwt = require('jsonwebtoken');
 app.use(cors());
 app.use(express.json());
 
+// JWT ServerSite::Step=2 create a verify function  
+const verifyJWTToken = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" });
+    };
+    // IN genaral we will get token from client site as 'bearer token' 
+    // * so we split here the authorization which we take from client site headers
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y1dis5d.mongodb.net/?retryWrites=true&w=majority`;
@@ -35,19 +53,48 @@ async function run() {
         const CartCollecttion = client.db('MasuksKitchenDB').collection('carts');
 
 
-        // JWT Functionality Here 
+
+        // To Verify Admin MiddleWare 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden access' });
+            }
+            next();
+        }
+
+
+        // JWT Serversite::STEP=1 At First npm install jwt then require jwt then start from here 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '10h' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "10h" })
             res.send({ token });
         })
 
         // User Handle API start Here 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWTToken, verifyAdmin, async (req, res) => {
             const cursor = usersCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        // Check a User if he/she Admin or Not 
+        app.get('/user/:email', verifyJWTToken, async (req, res) => {
+            const email = req.params?.email;
+
+            if (req.decoded.email !== email) {
+                // res.status(403).send({error: true, message: "unauthorized access"}) 
+                // or 
+                res.send({ admin: false });
+            }
+
+            query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role == 'admin' };
+            res.send(result);
+        })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -60,6 +107,7 @@ async function run() {
             res.send(result);
         });
 
+        // Set A User As Admin API
         app.patch('/users/:id', async (req, res) => {
             const id = req.params?.id;
             const filter = { _id: new ObjectId(id) };
@@ -97,11 +145,17 @@ async function run() {
         });
 
 
-        // handle Cart Items API down from here
+        // JWT Serversite:STEP=3 use verifyJWTToken to those API's, you want to secure
 
-        app.get('/carts', async (req, res) => {
+        // handle Cart Items API down from here
+        app.get('/carts', verifyJWTToken, async (req, res) => {
             let query = {};
             const email = req.query?.email;
+            //JWT Serversite::STEP=4 check the token bearer and the user who requested for are the same 
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' });
+            }
             if (req.query?.email) {
                 query = { email: email };
             }
